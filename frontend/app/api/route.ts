fdd5dcb209de6bbe0c74de052562d4e5f3d7bcfe
yabu1121 <n.hayato0122@outlook.jp>
2025-12-14 18:43:59 +0900

    reset

--- frontend/app/api/route.ts
+++
@@ -1,7 +0,0 @@
-
-export default Todo = async () => {
-  const res = await fetch("http://127.0.0.1");
-  const data = await res.json();
-  console.log(res);
-  console.log(data);
-}
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,9 +1,6 @@
 const TopPage = async () => {
-  const res = await fetch("http://127.0.0.1:8000")
-  const data = res.json(); 
-  console.log(data);
   return (
-    <div>p</div>
+    <div>top</div>
   )
 }
 
