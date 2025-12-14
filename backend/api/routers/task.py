d28ec92923e5a81025dcb71f5e1c80d7897bd7bc
yabu1121 <n.hayato0122@outlook.jp>
2025-12-15 01:12:36 +0900

    schema request

--- backend/api/routers/task.py
+++ backend/api/routers/task.py
@@ -12,14 +12,17 @@ router = APIRouter()
 async def list_tasks():
   return [task_schema.Task(id=1, title="1つ目のtodoタスク")]
 
-@router.post("/tasks")
-async def create_tasks():
-  pass
+# create_task関数の引数にtask_bodyというのを入れて、dictに変換して、
+# **をつけることでキーワード引数として展開する
+# これはdictをkey/valueわたしをするから、数が多くなってきたときに役立つ
+@router.post("/tasks", response_model = task_schema.TaskCreateResponse)
+async def create_task(task_body : task_schema.TaskCreate):
+  return task_schema.TaskCreateResponse(id=1, **task_body.dict())
 
-@router.put("/tasks/{task_id}")
-async def update_tasks():
-  pass
+@router.put("/tasks/{task_id}", task_schema.TaskCreateResponse)
+async def update_tasks(task_id: int , task_body : task_schema.TaskCreate):
+  return task_schema.TaskCreateResponse(id=task_id, **task_body.dict())
 
 @router.delete("/tasks/{task_id}")
-async def delete_tasks():
-  pass
+async def delete_tasks(task_id: int):
+  return 
--- backend/api/schemas/task.py
+++ backend/api/schemas/task.py
@@ -1,13 +1,30 @@
 from typing import Optional
-
 from pydantic import BaseModel, Field
 
 
-class Task(BaseModel):
-    # int の型ヒント
-    id: int
-    # Optional[str] という型ヒント
-    # Fieldはフィールドに関する付加情報を加える。第一引数は初期値、第二引数は値の例
-    title: Optional[str] = Field(None, example = "クリーニングを取りに行く")
-    # bool という型ヒント
-    done: bool = Field(False, description = "完了フラグ")
+# BaseModelを継承してTaskBaseを作っている。
+class TaskBase(BaseModel):
+  # Optionalは指定した型、もしくはNoneを許容する。
+  # typing.Unionは複数の型を指定可能。
+  # typingライブラリは型付けのライブラリである。
+  title: Optional[str] = Field(None, example = "クリーニングを取りに行く")
+    
+
+class TaskCreate(TaskBase):
+  pass
+
+class TaskCreateResponse(TaskCreate):
+  id: int
+  class Config:
+    orm_mode = True
+
+# TaskBaseを継承してTaskを作っている。
+class Task(TaskBase):
+  # int の型ヒント
+  id: int
+  # Optional[str] という型ヒント
+  # Fieldはフィールドに関する付加情報を加える。第一引数は初期値、第二引数は値の例
+  # bool という型ヒント
+  done: bool = Field(False, description = "完了フラグ")
+  class Config:
+    orm_mode = True
